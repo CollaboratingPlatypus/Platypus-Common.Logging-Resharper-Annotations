f77b5e64e26036a281a2ce09fea4c526e407a570
Wade Baglin <plebmail@gmail.com>
2015-03-14 20:01:42 +1000

    Fixes a minor uninstall bug

--- Tools/Uninstall.ps1
+++ Tools/Uninstall.ps1
@@ -1,7 +1,7 @@
 param($installPath, $toolsPath, $package, $project)
 
 $packagesDirectory = (Get-Item $installPath).parent
-$installPaths = Get-ChildItem $packagesDirectory.FullName -Recurse -File | Where-Object { $_.Name -ieq "Common.Logging.Core.ExternalAnnotations.xml" }
+$installPaths = Get-ChildItem $packagesDirectory.FullName -Recurse -File | Where-Object { $_.Name -ieq "Common.Logging.Core.ExternalAnnotations.xml" -and $_.FullName -notlike "*Platypus.Common.Logging*" }
 
 foreach($installPath in $installPaths)
 {
